6070126f160a34b10b53cc5861afeddc5159b621
priyakt2022 <98765198+priyakt2022@users.noreply.github.com>
2022-02-26 23:33:45 -0500

    Create calculator.groovy

---
+++ vars/calculator.groovy
@@ -0,0 +1,12 @@
+def add(x,y){
+  print "sum of ${x} and ${y} is ${x+y}"
+}
+ def SUB(x,y){
+  print "sub of ${x} and ${y} is ${x-y}"
+}
+def MUL(x,y){
+  print "mul of ${x} and ${y} is ${x*y}"
+}
+
+
+
